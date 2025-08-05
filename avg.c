f6958f2a7e80e80bef76b028a05eb4bc10ded6f9
Druthika-mekala <druthikamekala09@gmail.com>
2025-08-05 09:09:57 +0530

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,13 @@
+#include<stdio.>
+int number(int a, int b){
+int sum=0
+sum=a+b;
+int avg=sum/2;
+return avg;}
+int main(){
+int x,y;
+scanf("%d %d"'&x,&y);
+int result=avg(x,y);
+printf("%d",result);
+}
+return 0;
