33933f657227be5d1d1ca178d2dd555e00ef8dbc
Druthika-mekala <druthikamekala09@gmail.com>
2025-08-05 09:30:17 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int area(int l,int b){
+int area=o;
+area=l*b;
+return area;}
+int main(){
+int x,y;
+scanf("%d %d",&x,&y);
+int result=area(x,y);
+printf("%d",result);
+return 0;
+}
+
