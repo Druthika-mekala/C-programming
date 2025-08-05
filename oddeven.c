39971f78a67f7493fdbe4649d798ad770e77483a
Druthika-mekala <druthikamekala09@gmail.com>
2025-08-05 09:57:53 +0530

    Create oddeven.c

---
+++ oddeven.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int N;
+printf("Enter the number");
+scanf("%d",&N);
+if(N%2==0)
+printf("Even");
+else if(N%2!=0)
+printf("odd");
+else
+printf(error);
+return 0;
+}
